359326964f99037cdaeeee88dfc2868db8d24963
Zachary Jordan <zachary.jordan@my.whccd.edu>
2025-10-24 22:52:56 -0700

    script2 completed, fizzbuzz fairly easy

--- script1.js
+++ script1.js
@@ -8,6 +8,8 @@
 // ##
 // #
 
+
+//maybe i should've done height = 7 and start the loops at 1, but this is just what i know
 let height = 8;
 
 //i was trying to do nested for loops before like I did with C, but the repeat doesnt require that
--- script2.js
+++ script2.js
@@ -9,3 +9,20 @@
 // 14
 // FizzBuzz
 // ...
+
+
+for(let i = 1; i <= 100; i++){
+    //order matters, i think start with fizzbuzz? if i % 5 and i % 3 console.log fizbuzz, else if i % 3 do fizz, else if 1 % 5 do buzz, else console.log i
+
+    if (i % 5 == 0 && i % 3 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+//had one error where i didnt do the == 0 and all the answers were backwards, but this one was pretty easy actually, i hope i did it right though
