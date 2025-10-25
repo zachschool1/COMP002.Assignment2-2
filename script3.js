6e25be885dbb8ca9ccc3bd681af9c85450ed99f1
Zachary Jordan <zachary.jordan@my.whccd.edu>
2025-10-24 23:02:43 -0700

    finish script3 in a simple way

--- script3.js
+++ script3.js
@@ -9,3 +9,18 @@
 //  # # # #
 // # # # #
 //  # # # #
+
+
+
+//so basically have to check if its space # or # space in the loop, maybe a switch case could be used here since there are only 2 options, this one seems hard. i think looping string concatenation might be a way to do this also but im not sure how i would get it to alternate, maybe theres another thing like
+
+for(let i = 1; i <= 8; i++){
+    if (i % 2 == 0) {
+        console.log("# # # #");
+    } else {
+        console.log(" # # # #");
+    }
+}
+
+
+//im not sure if this is the 'best' way to do it, but it works so I'm not going to complain lol.
